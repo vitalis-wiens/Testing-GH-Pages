46474a2ae18a73b7458a72a7ecf570ec0424a460
Vitalis Wiens <vitalis.wiens@gmail.com>
2021-07-02 18:00:09 +0200

    Updates

---
+++ precache-manifest.5ac1046acf30bde6590836d3f182e35a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131f4862ce7e9cb3c123bd55a64a72e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6d55e0716e1801a1414",
+    "url": "./static/css/2.78063165.chunk.css"
+  },
+  {
+    "revision": "0d4c75c5efa98ed66129",
+    "url": "./static/css/main.b04fd904.chunk.css"
+  },
+  {
+    "revision": "a6d55e0716e1801a1414",
+    "url": "./static/js/2.0a47c62b.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "./static/js/2.0a47c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4c75c5efa98ed66129",
+    "url": "./static/js/main.183c7f35.chunk.js"
+  },
+  {
+    "revision": "0bb4eb37e09b80c79523",
+    "url": "./static/js/runtime-main.7f568536.js"
+  },
+  {
+    "revision": "7d352eef4fc0d24048db63b8a4644f4e",
+    "url": "./static/media/logo.7d352eef.svg"
+  }
+]);
